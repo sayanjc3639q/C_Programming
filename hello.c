67ad7e3bfbd1418c48dca2f14a22f85ecdc1e060
Sayan Maity <jcsayan7@gmail.com>
2025-06-17 14:20:53 +0530

    Hello World

--- hello.c
+++ hello.c
@@ -2,14 +2,7 @@
 
 int manin(){
 
-    int age = 20;
-
-    int *ageptr;
-
-    ageptr = &age; // Assign the address of age to ageptr
-
-    printf("Age: %d\n", *ageptr); // Dereference ageptr to get the value of age
-    printf("Address of age");
+    printf("Hello, World!\n");
 
     return 0;
 }
