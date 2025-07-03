eb9fcd54223fd01fd8edc3b422233160cb4b6ad6
Sayan Maity <jcsayan7@gmail.com>
2025-07-03 20:45:58 +0530

    added a file 002_sum_two_numbers.c

---
+++ 01_Basics/002_sum_two_numbers.c
@@ -0,0 +1,18 @@
+// 2. Sum of two numbers
+#include <stdio.h>
+
+int main(){
+    // declare variables 
+    int a = 5; // we have to put this value
+    int b = 8; // we have to put this value
+    int sum; // we are expected to get this value
+
+    sum = a + b; // we do not need to declare the variable again
+
+    // now we will print the sum we have calculated
+    printf("The sum is : %d ",sum); //printf(" %_format_specifier_of_variable",varibale)
+    // we must need to declare the varible type before printing here we get integer so we use %d
+    // for character %c and for float %f
+
+    return 0;
+}
