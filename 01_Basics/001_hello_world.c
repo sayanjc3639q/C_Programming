37e9cb0736aaa006486651267853370c573348fd
Sayan Maity <jcsayan7@gmail.com>
2025-07-03 20:35:16 +0530

    new file

---
+++ 01_Basics/001_hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //include standard input and output library
+// include tells the compiler to include the header file
+
+// int -->(return type)  main(function parameter) --> function
+/*  we must nedd main() in function C programs in a hosted environment 
+    (like those running on an operating system) there are excpetions*/
+int main(){ // main function where the acctual code will be written 
+    // printing syntax printf(const char *__format, ...)
+    printf("Hello Word"); // must use a semi colon for statement terminator
+
+    return 0; // return 0 for succesful program execution
+}
+
+// NOTE : Return type of main function is integer ( we will return 0 for succesful execution)
