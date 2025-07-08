27f5faca97f6f14da8ee6dbd063bbfe84cec437b
Sayan Maity <jcsayan7@gmail.com>
2025-07-08 17:35:28 +0530

    032_fibonacci_recursive.c added

---
+++ 04_Functions/032_fibonacci_recursive.c
@@ -0,0 +1,22 @@
+//32. Use recursion for Fibonacci
+// Recursion : it is a type of function syntax where we call the function inside a function to solve problem
+#include <stdio.h>
+
+int fibonacci(int n);
+
+int main(){
+    int input;
+    printf("Enter the nth term upto which you want to print the fibonacci:");
+    scanf("%d",&input);
+
+    for(int i = 0; i <= input ; i++){
+        printf("%d  ",fibonacci(i));
+    }
+}
+
+int fibonacci(int n){
+    //declare the base case
+    if(n == 0){return 0;} 
+    else if (n == 1){return 1; }
+    else{ return fibonacci(n-1) + fibonacci(n-2);}
+}
